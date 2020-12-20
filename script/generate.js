8f6c519eef3cc1b074286c09c5abfe24c02a8812
TiM-SyStEm <72683799+TiM-SyStEm@users.noreply.github.com>
2020-12-20 22:05:56 +0700

    Delete generate.js

--- script/generate.js
+++
@@ -1,9 +0,0 @@
-//Generate range
-function randomInt(min, max) {
-  return Math.floor(Math.random() * (max - min + 1)) + min;
-}
-function speak() {
-    let text = randomInt(1,9999999999999);
-    speechSynthesis.speak(new SpeechSynthesisUtterance(text));
-}
-speak();
